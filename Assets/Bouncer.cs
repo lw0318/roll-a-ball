1dc86f03a3a9bb81e654e17bdb7ff406192993a7
lw0318 <60265673+lw0318@users.noreply.github.com>
2020-03-02 11:54:12 -0600

    3/2

---
+++ Assets/Bouncer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    [SerializeField]
+    public float bouncepower = 100;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        PlayerController pc = collision.gameObject.GetComponent<PlayerController>();
+
+        Vector3 bounce = Vector3.Reflect(pc.lastvelocity, collision.contacts[0].normal);
+        Debug.Log("velocity" + rb.velocity);
+        Debug.Log("bounce" + bounce * bouncepower);
+
+        rb.AddForce(bounce.normalized * bouncepower);
+    }
+}
